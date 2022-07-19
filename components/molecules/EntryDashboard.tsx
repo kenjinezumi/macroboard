3ca74776714032a37f57d4700de143c614f58932
Kenji Tsuchiya <kenji.tsuchiya@blackrock.com>
2022-07-19 12:16:12 +0100

    Add entry page

---
+++ components/molecules/EntryDashboard.tsx
@@ -0,0 +1,36 @@
+import Link from "../../node_modules/next/link";
+import "bootstrap/dist/css/bootstrap.css";
+import { Graph } from "../atoms/RenderGraphs";
+import dynamic from "../../node_modules/next/dynamic";
+
+const LeafLetChart = dynamic(() => import("../atoms/Map"), { ssr: false });
+
+const ExternalLink = ({ href, children }) => (
+  <a
+    className="text-gray-500 hover:text-gray-600 transition"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}
+  >
+    {children}
+  </a>
+);
+
+export default function entryDashboard() {
+  return (
+    <div className="col bg-secondary overflow-auto position-relative">
+      <div className="h-100 w-100 d-flex flex-column">
+        <div className="row flex-grow-1 m-4 align-items-center justify-content-center h-25 w-100">
+          <div className="col-md-6 d-flex h-50 w-50 justify-content-center">
+            <div className="h-50 w-50 text-center bg-dark">HELLO</div>
+          </div>
+          <div className="col-md-6  h-50 w-50 align-items-center justify-content-center">
+            <div className=" h-50 w-50 align-items-center text-center bg-dark">
+              HELLO
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -12,7 +12,7 @@ const ExternalLink = ({href, children}) =>(
 
 export default function navbar(){
     return (
-        <div className="container-fluid position-fixed">
+        <div className="container-fluid">
        
                 <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
